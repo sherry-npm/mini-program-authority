a6e87ef0b3820fccabcf9006c38fe5b53bac9a43
shryzhang <996707719@qq.com>
2019-07-20 17:43:16 +0800

    DEV:api重设计&代码逻辑、结构优化

--- src/index.js
+++ src/index.js
@@ -8,143 +8,75 @@ const user = {};
 /* ***** 存储不需要被外部访问的方法 ***** */
 const privates = {
 	token: '',
-	userInfo: '',
+	userInfo: {},
 	phone: ''
 };
 
-/* ********** 外部方法定义 ********** */
-
-// 检查用户是否在登录态
-user.checkLogin = () => {
-	// 如果本次代码运行，已检查过登录状态，则token有值。直接取即可
-	if (privates.token && privates.userInfo) {
-		return Promise.resolve(privates.userInfo);
-	} else {
-		// 检查storage是否存有token
-		return privates.checkStorage().then(token => {
-			// 如果在登录态
-			if (token) {
-				// 检查token是否过期
-				return privates.checkSession().then(isOutDate => {
-					// token已过期
-					if (isOutDate) {
-						// 重新执行登录
-						return privates.login().then(rs => {
-							if (rs && rs.data) {
-								const newToken = rs.data.token;
-								// 缓存token
-								return privates.tokenStorage(newToken).then(() => {
-									// 前端本地缓存token(代码里缓存)
-									privates.token = newToken;
-									// 检查本地服务器，是否已存有用户信息
-									return user.checkUserInfo(newToken);
-								})
-							} else {
-								return Promise.reject(rs.msg || '登录失败');
-							}
-						})
-					} else {
-						// token未过期
-						// 前端本地缓存token(代码里缓存)
-						privates.token = token;
-						// 检查本地服务器，是否已存有用户信息
-						return user.checkUserInfo(token);
-					}
-				})
-			} else {
-				// 如果不在登录态
-				// 检查用户是否已经授权获取用户信息（需要先授权，再登录）
-				return privates.checkUserInfoAuth('checkLogin');
-			}
-		}).catch(err => {
-			// 在最外层promise catch掉本层($wxp.getStorage)及内部所有错误，并返回给页面
-			return Promise.reject(err);
-		});
-	}
-}
-
-
-
-// 检查本地服务器是否存在用户信息
-user.checkUserInfo = () =>
-	privates.getUserInfo().then(userInfo => {
-		// 本地服务器已存在用户信息
-		if (userInfo) {
-			// 前端本地缓存用户信息
-			privates.userInfo = userInfo;
-			// 直接返回用户信息
-			return userInfo;
-		} else {
-			// 本地服务器无用户信息。检查用户信息是否已授权获取
-			return privates.checkUserInfoAuth('checkUserInfo');
-		}
-	});
-
-// 检查本地服务器是否存在用户手机号
-user.checkPhone = () => privates.getPhone().then(rs => {
-	// 本地服务器已存在用户手机号
-	if (rs) {
-		// 前端本地缓存用户手机号
-		privates.phone = rs;
-		// 直接返回
-		return rs;
-	} else {
-		// 本地服务器无用户手机号。前往授权页请求授权手机号
-		return privates.navigateToAuthPage('phone');
-	}
-});
-
-
-// 提供外部获取用户token的方法
-user.getToken = function () {
-	return privates.token;
-};
-
-// 提供外部获取用户信息的方法
-user.getUserInfo = function () {
-	return privates.userInfo;
-};
-
-// 提供外部获取用户手机号的方法
-user.getPhone = function () {
-	return privates.phone;
-};
-
-
-
 /* ********** 私有方法定义 ********** */
 
-// 缓存token
-privates.tokenStorage = token => $wxp.setStorage({
+// 检查storage 返回 token/''
+privates.checkStorage = () => $wxp.getStorage({
+	key: 'token'
+}).then(data => data).catch(() => '');
+
+// 检查用户授权信息 返回 true/false
+privates.isUserInfoAuth = () =>
+	$wxp.getSetting().then(
+		res => res.authSetting['scope.userInfo']
+	).catch(
+		err => Promise.reject(`[from wechat catch]检查用户授权信息失败：${err}`)
+	);
+
+// 检查token是否过期 返回 true/false
+privates.checkSession = () => $wxp.checkSession().then(() => false).catch(() => true);
+
+// 微信登录 返回 code
+privates.wxLogin = () =>
+	$wxp.login().then(
+		res => res && res.code
+	).catch(
+		err => Promise.reject(`[from wechat catch]执行微信登录失败：${err}`)
+	);
+
+// 开发者服务器登录 返回 token
+privates.login = () => privates.wxLogin().then(code => {
+	if (code) {
+		return $wxp.request({
+			url: privates.loginApi,
+			method: 'get',
+			data: {
+				code
+			}
+		}).then(rs => {
+			if (rs && rs.data) {
+				return rs.data.token;
+			} else {
+				return Promise.reject(rs.msg || rs.message || '登录失败');
+			}
+		}).catch(
+			err => Promise.reject(`[from catch]登录失败：${err}`)
+		);
+	} else {
+		return Promise.reject(`[from wechat]登录失败：${res.errMsg}`);
+	}
+});
+
+// storage缓存token 无返回值 成功走then，失败走catch
+privates.saveToken = token => $wxp.setStorage({
 	key: 'token',
 	data: token
-});
+}).catch(
+	err => Promise.reject(`[from wechat catch]token缓存失败：${err}`)
+);
 
-// 获取用户信息（微信）
+// 获取微信用户信息
 privates.getWxUserInfo = () => $wxp.getUserInfo({
-	// 获取包含 encryptedData, iv 等敏感信息的 userInfo
-	withCredentials: true,
 	lang: 'zh_CN'
-});
-
-// 用户信息初始化
-privates.userInfoInit = () =>
-	privates.getWxUserInfo().then(wxUserInfo => {
-		// 如果用户提供了本地服务器存储用户信息的方法，则调用，将用户信息存储到本地服务器
-		if (privates.setUserInfo) {
-			return privates.setUserInfo(wxUserInfo.userInfo).then(rs => {
-				if (rs) {
-					privates.userInfo = wxUserInfo.userInfo;
-					return wxUserInfo.userInfo;
-				} else {
-					return Promise.reject('用户信息存储失败，请稍后再试');
-				}
-			})
-		} else {
-			privates.userInfo = wxUserInfo.userInfo;
-			return wxUserInfo.userInfo;
-		}
-	});
+}).then(
+	res => res && res.userInfo
+).catch(
+	err => Promise.reject(`[from wechat catch]获取用户信息失败：${err}`)
+);
 
 // 跳转到用户授权页
 privates.navigateToAuthPage = authType => {
@@ -158,62 +90,116 @@ privates.navigateToAuthPage = authType => {
 	return Promise.reject('未获取用户授权');
 };
 
-// 检查用户信息授权状态
-privates.checkUserInfoAuth = caller => privates.isUserInfoAuth().then(isAuth => {
-	// 如果用户已授权访问用户信息
-	if (isAuth) {
-		if (caller === 'checkLogin') {
-			// 直接执行登录
-			return privates.login().then(rs => {
-				if (rs && rs.data) {
-					const token = rs.data.token;
-					// 缓存token
-					return privates.tokenStorage(token).then(() => {
-						// 前端本地缓存token(代码里缓存)
-						privates.token = token;
-						// 获取微信用户信息
-						return privates.userInfoInit();
-					})
+
+// 用户信息初始化
+privates.userInfoInit = () =>
+	privates.getWxUserInfo().then(wxUserInfo => {
+		// 如果用户提供了本地服务器存储用户信息的方法，则调用，将用户信息存储到本地服务器
+		if (privates.setUserInfo) {
+			return privates.setUserInfo(wxUserInfo).then(rs => {
+				if (rs) {
+					privates.userInfo = wxUserInfo;
+					return wxUserInfo;
 				} else {
-					return Promise.reject(rs.msg || '登录失败');
+					return Promise.reject('用户信息存储失败，请稍后再试');
 				}
 			})
 		} else {
-			// 获取微信用户信息
-			return privates.userInfoInit();
+			privates.userInfo = wxUserInfo;
+			return wxUserInfo;
 		}
+	});
+
+
+// 执行登录
+privates.doLogin = callback => privates.login().then(token => {
+	// 前端本地缓存token(代码里缓存)
+	privates.token = token;
+	// 缓存token
+	return privates.saveToken(token).then(() => {
+		return callback(token);
+	})
+})
+
+// 检查用户信息授权状态
+privates.checkUserInfoAuth = () => privates.isUserInfoAuth().then(isAuth => {
+	// 如果用户已授权访问用户信息
+	if (isAuth) {
+		// 直接执行登录
+		return privates.doLogin(privates.userInfoInit);
 	} else {
 		// 未授权访问用户信息。前往授权页请求授权用户信息
 		return privates.navigateToAuthPage('userInfo');
 	}
 });
 
-// 检查用户是否已授权访问用户信息
-privates.isUserInfoAuth = () => $wxp.getSetting().then(res => res.authSetting['scope.userInfo']);
+// 检查本地服务器是否存在用户信息
+privates.checkUserInfo = token =>
+	privates.getUserInfo(token).then(userInfo => {
+		// 开发者服务器已存在用户信息
+		if (userInfo) {
+			// 前端本地缓存用户信息
+			privates.userInfo = userInfo;
+			// 直接返回用户信息
+			return userInfo;
+		} else {
+			// 开发者服务器无用户信息，直接获取微信用户信息（执行到这里，走的一定是storage已有token分支。有token说明授权登录过，不必再次请求授权）
+			return privates.userInfoInit();
+		}
+	});
 
-// 检查storage是否存有token
-privates.checkStorage = () => $wxp.getStorage({
-	key: 'token'
-}).then(storage => storage).catch(() => '');
 
-// 检查token是否过期
-privates.checkSession = () => $wxp.checkSession().then(() => false).catch(() => true);
+/* ********** 外部方法定义 ********** */
 
-// 执行登录
-privates.login = () => $wxp.login().then(res => {
-	if (res.code) {
-		return $wxp.request({
-			url: privates.loginApi,
-			method: 'get',
-			data: {
-				code: res.code
+// 获取token的方法
+user.getToken = () => privates.token;
+
+// 检查用户登录状态 返回 userInfo
+user.checkLogin = () => {
+	// 如果本次代码运行，已检查过登录状态，则token有值。直接取即可
+	if (privates.token && privates.userInfo) {
+		return Promise.resolve(privates.userInfo);
+	} else {
+		return privates.checkStorage().then(token => {
+			if (token) {
+				// 检查token是否过期
+				return privates.checkSession().then(isExpired => {
+					// token未过期，直接拉取开发者服务器用户信息
+					if (!isExpired) {
+						privates.token = token;
+						// 检查开发者服务器是否存有用户信息
+						return privates.checkUserInfo(token);
+					} else {
+						// token过期，重新执行登录
+						return privates.doLogin(privates.checkUserInfo);
+					}
+				});
+			} else {
+				return privates.checkUserInfoAuth();
 			}
 		});
-	} else {
-		return Promise.reject(`登录失败！${res.errMsg}`);
 	}
-});
+}
 
+// 检查用户手机号 返回 phone
+user.checkPhone = () => {
+	if (privates.phone) {
+		return Promise.resolve(privates.phone);
+	} else {
+		return privates.getPhone().then(rs => {
+			// 本地服务器已存在用户手机号
+			if (rs) {
+				// 前端本地缓存用户手机号
+				privates.phone = rs;
+				// 直接返回
+				return rs;
+			} else {
+				// 本地服务器无用户手机号。前往授权页请求授权手机号
+				return privates.navigateToAuthPage('phone');
+			}
+		});
+	}
+}
 
 // 对象初始化（工具包入口）
 user.init = ({
